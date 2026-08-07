44ac1d6a816d1126df79241a084ffacce003c613
rajunaga4305-afk <rajunaga4305@gmail.com>
2026-08-07 11:35:34 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,12 @@
+s="janu" 
+for x in s : 
+     print(x)
+     
+d1={'AP':'Amravati',2:4}
+for x,y in d1.items():
+	print(x,y)
+
+d2={2:4,4:16,8:64}
+total=0
+for x,y in d2.items():
+	print(total+x+y)
