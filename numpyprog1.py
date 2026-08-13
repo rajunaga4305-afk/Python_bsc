5f773b81940717da78190bac2e4474dfb85c0299
rajunaga4305-afk <rajunaga4305@gmail.com>
2026-08-13 12:10:11 +0530

    Add files via upload

---
+++ numpyprog1.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+s1 = np.array([[1,2,3,4,5], [3,5,6,2,1]])
+
+print()
+print(s1)
+print()
+
+d2 = s1.shape
+print("The result is:", d2)
+print()
+
+print("The daimentions are:", s1.ndim)  # "dimensions" is misspelled in the photo
+print()
